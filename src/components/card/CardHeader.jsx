5146720adf7bdfbfdd5c03f7079a56a555584c9e
Franco Lorca <lorcafrancog@gmail.com>
2022-12-26 17:48:28 -0300

    Update
    Added interactivity

--- src/components/card/CardHeader.jsx
+++ src/components/card/CardHeader.jsx
@@ -1,7 +1,21 @@
+import {useContext} from "react";
+import {CardContext} from "../../context/CardContext.jsx";
+
 export const CardHeader = () => {
-    return (
-        <div className={"card__header"}>
-            <img src="/icon-star.svg" alt="Icon star" className={"header__img"}/>
-        </div>
-    )
+    const {submitState, currentNumber} = useContext(CardContext)
+
+    if (!submitState) {
+        return (
+            <section className={"card__header"}>
+                <img src="/icon-star.svg" alt="Icon star" className={"header__img"}/>
+            </section>
+        )
+    } else {
+        return (
+            <section className={"card__header alter"}>
+                <img src="/illustration-thank-you.svg" alt="Illustration thank you"/>
+                <div className="header__stars"><p>You selected {currentNumber} out of 5</p></div>
+            </section>
+        )
+    }
 }
