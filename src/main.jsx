d74f1c8613caea8fcdd1e08afacb6111ffc900e3
Franco Lorca <lorcafrancog@gmail.com>
2022-12-28 15:42:13 -0300

    Update
    Removed unused import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import {CardContext, CardContextProvider} from "./context/CardContext.jsx";
+import {CardContextProvider} from "./context/CardContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
